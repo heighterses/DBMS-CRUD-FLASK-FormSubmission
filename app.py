836e5c3f68bca4e082cf5e77c85200891233b390
M. Abdullah Bin Aftab <heighter79@gmail.com>
2024-01-22 11:51:14 +0500

    day 4

--- app.py
+++ app.py
@@ -19,12 +19,14 @@ with app.app_context():
     db.create_all()
 
 # APPLICATION CODE
-# all_books = []
+all_books = []
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    books = BOOKS.query.all()
+
+    return render_template('index.html', books=all_books)
 
 
 @app.route('/add', methods=["POST", "GET"])
@@ -35,21 +37,36 @@ def add_book():
         author = data["author"]
         rating = data["rating"]
 
-        # new_book = {
-        #     "title": data["name"],
-        #     "author": data["author"],
-        #     "rating": data["rating"]
-        # }
-        # all_books.append(new_book)
+        new_book = {
+            "title": data["name"],
+            "author": data["author"],
+            "rating": data["rating"]
+        }
+        all_books.append(new_book)
 
         with app.app_context():
-            enter_books_data = BOOKS(name=title,author=author,rating=rating)
+            enter_books_data = BOOKS(name=title, author=author, rating=rating)
             db.session.add(enter_books_data)
             db.session.commit()
 
         return redirect(url_for('home'))
 
     return render_template('add_book.html')
+
+
+@app.route('/delete/<string:name>', methods="GET")
+def delete_book(name):
+    delete_book = BOOKS.query.filter_by(name=name).first()
+
+    if delete_book:
+        db.session.delete(delete_book)
+        db.session.commit()
+        flash(f'the book {name} is deleted succesfully')
+
+    else:
+        flash(f'Book with the name "{name}" not found!', 'danger')
+
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
